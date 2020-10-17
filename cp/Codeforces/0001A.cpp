2397622b2bc03622246bcc2ba08d2a01ab58407b
Soobeen Park <soobeenpark.dev@gmail.com>
2020-10-17 21:04:30 +0900

    Add codeforces 0001A

---
+++ cp/Codeforces/0001A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main() {
+    ll n, m, a;
+    ll hor, vert;
+
+    cin >> n >> m >> a;
+
+    if (n%a == 0) {
+        hor = n/a;
+    } else {
+        hor = n/a + 1;
+    }
+
+    if (m%a == 0) {
+        vert = m/a;
+    } else {
+        vert = m/a + 1;
+    }
+
+    cout << (hor * vert);
+}
