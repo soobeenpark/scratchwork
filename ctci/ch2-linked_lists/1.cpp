30519106310f74b7d4495531e82d4ecf9eb0d5ae
Soobeen Park <soobeenpark.dev@gmail.com>
2020-10-23 19:12:11 +0900

    Add CTCI ch2 ListNode.cpp and 2.1 for linked lists
    TODO: Test ListNode.cpp methods implementation.

---
+++ ctci/ch2-linked_lists/1.cpp
@@ -0,0 +1,105 @@
+#include <cassert>
+#include <iostream>
+#include <unordered_set>
+
+#include "ListNode.h"
+
+/* Let n be the length of the list.
+ * Time: O(n)
+ * Space: O(n)
+ */
+ListNode* removeDups(ListNode *head) {
+    std::unordered_set<int> set;
+    
+    ListNode *curr = head;
+    ListNode *prev = nullptr;
+    while (curr != nullptr) {
+        int val = curr->val;
+        if (set.count(val)) {
+            // val already seen!
+            assert(prev != nullptr);
+            prev->next = curr->next;
+
+            // Delete duplicate node
+            ListNode *tmp = curr;
+            curr = curr->next;
+            delete tmp;
+        } else {
+            // val first time
+            set.insert(val);
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+    return head;
+}
+
+/* Version with no extra space allowed (ie. Space complexity must be O(1)).
+ * Runtime suffers as a tradeoff.
+ *
+ * Let n be the length of the list.
+ * Time: O(n^2)
+ * Space: O(1)
+ */
+ListNode* removeDupsNoBuffer(ListNode *head) {
+    for (ListNode *curr = head; curr != nullptr; curr = curr -> next) {
+        ListNode *runnerPrev = curr;
+        ListNode *runner = curr->next;
+        while (runner != nullptr) {
+            if (runner->val == curr->val) {
+                runnerPrev->next = runner->next;
+
+                // Delete duplicate node
+                ListNode *tmp = runner;
+                runner = runner->next;
+                delete tmp;
+            } else {
+                runnerPrev = runner;
+                runner = runner->next;
+            }
+        }
+    }
+    return head;
+}
+
+
+int main() {
+    // Create Linked List
+    ListNode *head1 = new ListNode(1);
+    ListNode *trav = head1;
+    trav->next = new ListNode(2);
+    trav = trav->next;
+    trav->next = new ListNode(2);
+    trav = trav->next;
+    trav->next = new ListNode(2);
+    trav = trav->next;
+    trav->next = new ListNode(2);
+    trav = trav->next;
+    trav->next = new ListNode(6);
+    trav = trav->next;
+    trav->next = new ListNode(5);
+    trav = trav->next;
+    trav->next = new ListNode(5);
+    trav = trav->next;
+    trav->next = new ListNode(5);
+    trav = trav->next;
+    trav->next = new ListNode(2);
+    trav = trav->next;
+
+    ListNode *head2 = copyList(head1);
+
+    std::cout << "Testing v1" << std::endl;
+    printList(head1); // Before
+    removeDups(head1);
+    printList(head1); // After
+    std::cout << std::endl;
+
+    std::cout << "Testing v2" << std::endl;
+    printList(head2); // Before
+    removeDupsNoBuffer(head2);
+    printList(head2); // After
+    std::cout << std::endl;
+
+    deleteList(head1);
+    deleteList(head2);
+}
---
+++ ctci/ch2-linked_lists/ListNode.h
@@ -0,0 +1,64 @@
+#include <iostream>
+
+#ifndef LISTNODE_H
+#define LISTNODE_H
+
+// Define ListNode
+// Most interview practice problems I saw only use a Node class as linked list
+// interface instead of a full LinkedList wrapper class that includes a Node.
+// So I implemented it the same way without using a wrapper class.
+struct ListNode {
+    // Member methods
+    int val;
+    ListNode *next;
+
+    // Constructors
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* ptr) : val(x), next(ptr) {}
+    
+};
+
+// Print from current node to the end of list.
+void printList(ListNode *head) {
+    ListNode *curr = head;
+    while (curr != nullptr) {
+        std::cout << curr->val << " ";
+        curr = curr->next;
+    }
+    std::cout << std::endl;
+}
+
+// Make deep-copy of list.
+ListNode *copyList(ListNode *head) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+    // Create first node
+    ListNode* newHead = new ListNode(head->val);
+    head = head->next;
+
+    ListNode *prev = newHead; 
+    while (head != nullptr) {
+        ListNode *curr = new ListNode(head->val);
+        prev->next = curr;
+
+        // Update pointers
+        prev = curr;
+        head = head->next;
+    }
+    return newHead;
+}
+
+// Delete list to prevent memory leak.
+void deleteList(ListNode *head) {
+    ListNode *curr = head;
+    ListNode *prev = curr;
+    while (curr != nullptr) {
+        curr = curr -> next;
+        delete prev;
+        prev = curr;
+    }
+}
+
+#endif //LISTNODE_H
