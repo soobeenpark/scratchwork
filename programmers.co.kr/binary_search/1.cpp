b6e14fc2f7744986ce759e4fe775a2d64baa14e8
Soobeen Park <soobeenpark.dev@gmail.com>
2020-11-07 23:33:56 +0900

    Fix 1.cpp

--- programmers.co.kr/binary_search/1.cpp
+++ programmers.co.kr/binary_search/1.cpp
@@ -2,55 +2,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cassert>
-#include <iostream>
 
 using namespace std;
 
 typedef long long ll;
 
-int getMinInd(vector<int> timeLeft) {
-    auto it = min_element(timeLeft.begin(), timeLeft.end());
-    assert(*it >= 0);
-    return distance(timeLeft.begin(), it);
-}
-
-void updateTimes(vector<int> &timeLeft, int timeToElapse) {
-    for_each(timeLeft.begin(), timeLeft.end(), 
-            [&timeToElapse](int &t) { t -= timeToElapse; }
-    );
-}
-
 ll solution(int n, vector<int> times) {
-    vector<int> timeLeft(times.size(), 0);
-    ll answer = 0;
-    while (n > 0) {
-        // Get earliest next person who will finish processing 
-        int i = getMinInd(timeLeft);
-        answer += timeLeft[i];
+    // Sort for bin search to be used later
+    sort(times.begin(), times.end());
+    ll min = 0;
+    ll max = n * (ll)times[times.size() - 1];
+    ll answer;
 
-        // Assume the next person got processed - need to elapse time for rest
-        updateTimes(timeLeft, timeLeft[i]);
-
-        // Add new person
-        timeLeft[i] = times[i];
-        
-        // Deque person in line
-        n--;
-       
-        cout << "time " << answer << ": ";
-        for (int x : timeLeft) {
-            cout << x << " ";
+    while (min < max) {
+        ll mid = min + (max - min) / 2;
+        ll sum = 0;
+        for (ll t : times) {
+            sum += mid / t;
         }
-        cout << endl;
+
+        if (sum < n) {
+            // Not possible - move up min
+            min = mid + 1;
+        } else {
+            // Record answer and optimize for lower upper-bound
+            answer = mid;
+            max = mid;
+        }
     }
-
-    // Add any remaining people in line not finished yet
-    answer += *max_element(timeLeft.begin(), timeLeft.end());
     return answer;
-}
-
-int main() {
-    vector<int> times({7, 10});
-    cout << solution(6, times) << endl;
 }
