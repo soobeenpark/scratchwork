907818f3b58b94b2f18f6ac0551cedc740dfcb6f
Soobeen Park <soobeenpark.dev@gmail.com>
2020-11-08 23:03:39 +0900

    Add prgr.co.kr dfs_bfs 1 cpp

---
+++ programmers.co.kr/DFS_BFS/1.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int search(int k, const vector<int> &numbers, int sum, const int target) {
+    if (k == numbers.size()) {
+        return (int)(sum == target);
+    }
+    // Addition case
+    int ans = search(k+1, numbers, sum+numbers[k], target);
+    ans += search(k+1, numbers, sum-numbers[k], target);
+    return ans;
+}
+
+int solution(vector<int> numbers, int target) {
+    return search(0, numbers, 0, target);
+}
