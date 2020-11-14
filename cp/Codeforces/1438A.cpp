aa1aa9218bf05e34954204424299b1be8af12554
Soobeen Park <soobeenpark.dev@gmail.com>
2020-11-14 22:31:24 +0900

    Add codeforces 1438A

---
+++ cp/Codeforces/1438A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+        while (n--) {
+            cout << "1 ";
+        }
+        cout << endl;
+    }
+}
