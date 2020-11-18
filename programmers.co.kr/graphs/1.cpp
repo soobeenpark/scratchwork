ad17ad15e1acd1f72245b5043752ff15934e81ce
Soobeen Park <soobeenpark.dev@gmail.com>
2020-11-18 23:37:06 +0900

    Add prgr.co.kr graphs 1 cpp

---
+++ programmers.co.kr/graphs/1.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+const int START = 1;
+
+int bfs(int n, vector<int> adj[]) {
+    queue<pair<int, int>> q;  // <node, distance>
+    q.push(make_pair(START, 0));
+    vector<bool> visited(n+1, false);
+    visited[START] = true;
+    vector<int> distances(n+1, 0);
+    int distance = 0;
+
+    while (!q.empty()) {
+        pair<int, int> p = q.front();
+        q.pop();
+        int node = p.first;
+        distance = p.second;
+        distances[node] = distance;
+
+        for (int adjNode : adj[node]) {
+            if (!visited[adjNode]) {
+                visited[adjNode] = true;
+                q.push(make_pair(adjNode, distance+1));
+            }
+        }
+    }
+    // when we reach here, distance will always be max distance reachable by bfs
+    int count = 0;
+    for (int d : distances) {
+        count += (d == distance);
+    }
+    return count;
+}
+
+int solution(int n, vector<vector<int>> edges) {
+    // Create adjacency list
+    vector<int> adj[n+1];
+    for (vector<int> edge : edges) {
+        adj[edge[0]].push_back(edge[1]);
+        adj[edge[1]].push_back(edge[0]);
+    }
+
+    // bfs
+    return bfs(n, adj);
+}
+
